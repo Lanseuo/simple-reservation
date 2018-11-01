e895d9bcad3312b4c0030eea1d92a8856d8d0c1f
Lucas Hild <contact@lucas-hild.de>
2018-11-01 18:52:17 +0100

    Use minified Vue.js in production

--- inc/Base/Enqueue.php
+++ inc/Base/Enqueue.php
@@ -15,9 +15,13 @@ class Enqueue extends BaseController {
 
     function enqueue_frontend() {
         wp_enqueue_style( 'simple_reservation_style', $this->plugin_url . 'assets/frontend/style.css' );
-        // TODO: Production: https://vuejs.org/js/vue.min.js
-        wp_enqueue_script( 'simple_reservation_vuejs', 'https://vuejs.org/js/vue.js' );
-        wp_enqueue_script( 'simple_reservation_vuex', 'https://unpkg.com/vuex@2.0.0' );
+        if ( WP_DEBUG === true ) { 
+            wp_enqueue_script( 'simple_reservation_vuejs', 'https://vuejs.org/js/vue.js' );
+            wp_enqueue_script( 'simple_reservation_vuex', 'https://unpkg.com/vuex@2.0.0' );
+        } else {
+            wp_enqueue_script( 'simple_reservation_vuejs', 'https://vuejs.org/js/vue.min.js' );
+            wp_enqueue_script( 'simple_reservation_vuex', 'https://unpkg.com/vuex@2.0.0/dist/vuex.min.js' );
+        }
         wp_enqueue_script( 'simple_reservation_axios', 'https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js' );
         wp_enqueue_script( 'simple_reservation_script', $this->plugin_url . 'assets/frontend/store.js', null, null, true );
         wp_enqueue_script( 'simple_reservation_service', $this->plugin_url . 'assets/frontend/service.js' );
